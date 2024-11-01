5d38493fd819f720a00ff20d533f5920459c1fd4
techgauravtomar <olook306@gmail.com>
2024-11-01 19:39:00 +0530

    midlleware

--- src/app.js
+++ src/app.js
@@ -3,26 +3,30 @@ const app = express();
 
 
 app.use(
-  "/user", 
+  "/", 
   (req, res, next) =>{
   console.log("1st Response");
   // res.send({firstname: "Gaurav", lastname: "Tomar"});
   next();
-},
-(req, res, next)=>{
-  // res.send("2nd Response!")
-  next();
-},
-(req, res, next)=>{
-  // res.send("3nd Response!")
-  next();
-},
-(req, res, next)=>{
-  res.send("4nd Response!")
-}
+})
+
+app.get(
+  "/user",
+  (req,res,next)=>{
+    console.log("Handling / user route");
+    next();
+  },
+  (req,res,next)=>{
+    // res.send("Hello form the 1st Response!");
+    console.log("1st Response");
+    next();
+  },
+  (req,res,next)=>{
+    res.send("Hello form the 2nd Response!");
+    console.log("2nd Response");
+    // next();
+  }
 )
-
-
 
   // app.use((req, res) => {
   //   res.send("Hello form the server!")
